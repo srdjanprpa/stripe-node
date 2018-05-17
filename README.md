291eb74bb5e1ea876f99fb2407dcb0e78ecb602a
Alex Rattray <rattrayalex@stripe.com>
2018-05-16 18:00:42 -0700

    Initial stripe-node npm squat commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+Looking for the Stripe Node library?
+
+You want the [stripe](https://www.npmjs.com/package/stripe) package:
+
+```
+npm install --save stripe
+```
---
+++ index.js
@@ -0,0 +1,7 @@
+console.warn(
+  "\n⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️\n",
+  "\x1b[31m",
+  "\n`stripe-node` is not a package. Please `npm install stripe` instead.\n",
+  "\n⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️  ⚠️\n"
+);
+process.exit(1);
